a0749cd6aba7f37c1142e543f505777a4ca86b05
Karthik11045 <158801914+Karthik11045@users.noreply.github.com>
2024-02-04 20:04:16 +0530

    Update App.java

--- App.java
+++ App.java
@@ -9,7 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+                System.out.println( "Hello World hi there!" );
+        System.out.println( "Hello World hi there!" );
+        System.out.println( "Hello World hi there!" );
+
         System.out.println( "Hello World hi there!" );
         
     }
